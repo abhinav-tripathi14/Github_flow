f40987678d828f23680ccd1afaab3e18218997ed
Abhinav Tripathi <47190942+abhinav-tripathi14@users.noreply.github.com>
2023-03-14 11:38:57 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+## Terraform configuration
+
+terraform {
+  cloud {
+    organization = "Test-Abhinav"
+    workspaces {
+      name = "Github_flow"
+    }
+  }
+  required_version = ">= 1.1.0"
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.3.2"
+    }
+  }
+}
+
+variable "name_length" {
+  description = "The number of words in the pet name"
+  default     = "5"
+}
+
+resource "random_pet" "pet_name" {
+  length    = var.name_length
+  separator = "-"
+}
+
+output "pet_name" {
+  value = random_pet.pet_name.id
+}
